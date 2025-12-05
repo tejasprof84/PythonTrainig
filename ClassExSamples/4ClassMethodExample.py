144060df0b2ce78b1da881ee78306552d378a4bf
Tej <tejasprof84@gmail.com>
2025-12-05 18:46:30 +0530

    Adding Classmethod

---
+++ ClassExSamples/4ClassMethodExample.py
@@ -0,0 +1,45 @@
+class EmployeeData:
+    MinSalary = 100 #government given
+    Name=""
+    Salary=0
+
+    def __init__(self,name,salary):
+        self.Name=name
+        self.Salary=salary
+
+    def PrintEmpDetails(self):
+        calucatedsalaray=0
+        if self.Salary > self.MinSalary:
+            calucatedsalaray = self.Salary
+        else:
+            calucatedsalaray = self.MinSalary
+        print(f"name : {self.Name} salary {calucatedsalaray}")  
+
+   
+    @classmethod
+    def SetNewMinSalary(cls,salary):
+         cls.MinSalary = salary
+
+
+e1 = EmployeeData("Ram", 150)
+e2 = EmployeeData("Sham", 200)
+e3 = EmployeeData("Mam", 85)
+
+e1.PrintEmpDetails()
+e2.PrintEmpDetails()
+e3.PrintEmpDetails()
+
+
+#government oredered new min salary = 170
+EmployeeData.SetNewMinSalary(170)
+
+e1.PrintEmpDetails()
+e2.PrintEmpDetails()
+e3.PrintEmpDetails()
+
+
+
+
+
+
+    
